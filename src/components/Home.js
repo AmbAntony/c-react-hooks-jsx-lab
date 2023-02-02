1777fe9ffb1c51849912789a594c13d456d092b5
antony waweru <anto.waweru2030@gmail.com>
2023-02-02 06:13:53 +0300

    jsx aced

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home" style={{ color: "firebrick" }}>
+      <h1>
+        I am {name}, a Web Developer from {city}
+      </h1>
+      </div>
+
 }
 
 export default Home;
