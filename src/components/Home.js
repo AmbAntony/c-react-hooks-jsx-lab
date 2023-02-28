fd0b7150ae1ca8a3d4a06152841204b783763d7e
antony waweru <anto.waweru2030@gmail.com>
2023-02-28 05:42:01 +0300

    lab done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,15 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
+ 
   return(
-    
     <div id="home">
-      <h1 style={{ color: "firebrick" }}> I am {name}, a Web Developer from {city}</h1>
-    </div>
-  )
-
+      <h1 style={{color:"firebrick"}}> 
+      {`${name} a Web Developer from ${city}`}
+      </h1>
+    </div>  
+    
+  );
 }
 
 export default Home;
